6e2fe8fa4e4c48c235361c4ee2fd9204d91e5088
60071jimmy <60071jimmy@gmail.com>
2016-12-25 11:15:04 +0800

    Update data
    修正Form1.cs版本資訊日期錯誤
    README新增內容

--- UartOscilloscope/Form1.cs
+++ UartOscilloscope/Form1.cs
@@ -18,7 +18,7 @@
 ///                  Vision34：結構化錯誤訊息，建立Error_message_struct(錯誤訊息結構)，
 ///                            且將錯誤訊息內容封裝於Error_message_struct(錯誤訊息結構)中，外部無法任意修改，
 ///                            另錯誤訊息顯示不再直接呼叫MessageBox.Show，而是由Error_code_message類別中的Error_Message_Show副程式執行錯誤訊息顯示
-///   2016.12.23(六) Vision35：重新命名專案為UartOscilloscope
+///   2016.12.23(五) Vision35：重新命名專案為UartOscilloscope
 ///   未解決issue：
 ///   1、COM port中斷連線有時會導致程式當機
 ///   2、以Queue資料結構分析字串有時會發生錯誤
@@ -36,7 +36,7 @@ using System.Collections.Generic;                                               
 using System.Drawing;                                                           //  使用System.Drawing函式庫
 using SharpGL;                                                                  //  使用SharpGL函式庫(使用OpenGL函數)
 
-namespace UartOscilloscope                                              //  命名空間為本程式
+namespace UartOscilloscope                                                      //  命名空間為本程式
 {                                                                               //  進入命名空間
     public partial class Form1 : Form                                           //  Form1類別，繼承自System.Windows.Forms.Form類別
     {                                                                           //  進入Form1類別
---
+++ UartOscilloscope/README.md
@@ -0,0 +1,3 @@
+# UartOscilloscope
+版本資訊：
+2016.12.24(六)	調整命名空間(namespace)為UartOscilloscope
