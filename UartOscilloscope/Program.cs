145862a9cc804767a6356e30cce193af1d53a50b
60071jimmy <60071jimmy@gmail.com>
2017-01-01 19:53:58 +0800

    update annotation
    update annotation

--- UartOscilloscope/Program.cs
+++ UartOscilloscope/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;																	//	使用System函式庫
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;												//	使用System.Collections.Generic函式庫
+using System.Linq;																//	使用System.Linq函式庫
+using System.Threading.Tasks;													//	使用System.Threading.Tasks函式庫
 using System.Windows.Forms;														//	使用System.Windows.Forms函式庫
 
 namespace UartOscilloscope														//	命名空間為本程式
 {
-    static class Program                                                        //	Program類別
+    static class Program														//	Program類別
 	{																			//	進入Program類別
         /// <summary>
         /// 應用程式的主要進入點。
@@ -15,8 +15,8 @@ namespace UartOscilloscope														//	命名空間為本程式
         static void Main()														//	Main(主程式)
 		{																		//	進入Main(主程式)
 			Application.EnableVisualStyles();									//	為應用程式啟用視覺化樣式
-			Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-		}                                                                       //	結束Main(主程式)
-	}                                                                           //	結束Program類別
+			Application.SetCompatibleTextRenderingDefault(false);				//	為部分控制項上定義的 UseCompatibleTextRendering 屬性設定應用程式範圍的預設值
+			Application.Run(new Form1());										//	執行Form1副程式
+		}																		//	結束Main(主程式)
+	}																			//	結束Program類別
 }
