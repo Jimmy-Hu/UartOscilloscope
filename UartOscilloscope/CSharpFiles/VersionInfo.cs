b2fd59b07f13bc01ed13c4a7929ef1caef97b7ef
60071jimmy <60071jimmy@gmail.com>
2017-07-09 17:52:27 +0800

    新增VersionInfo.cs檔案
    新增VersionInfo.cs檔案

---
+++ UartOscilloscope/CSharpFiles/VersionInfo.cs
@@ -0,0 +1,21 @@
+﻿/********************************************************************
+ * Develop by Jimmy Hu												*
+ * This program is licensed under the Apache License 2.0.			*
+ * VersionInfo.cs													*
+ * 本檔案撰寫VersionInfo類別以紀錄程式版本資訊						*
+ ********************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartOscilloscope                                                      //	命名空間為本程式
+{                                                                               //	進入UartOscilloscope命名空間
+	class VersionInfo                                                           //	VersionInfo類別
+	{                                                                           //	進入VersionInfo類別
+		private static float Program_Version = 39;								//	宣告Program_Version靜態全域變數，記錄程式版本
+	}                                                                           //	結束VersionInfo類別
+}                                                                               //	結束UartOscilloscope命名空間
