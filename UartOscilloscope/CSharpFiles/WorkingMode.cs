7234661238ae926aa6347b46bfcefa83c8a0764a
60071jimmy <60071jimmy@gmail.com>
2017-07-09 17:51:32 +0800

    新增WorkingMode.cs檔案
    新增WorkingMode.cs檔案

---
+++ UartOscilloscope/CSharpFiles/WorkingMode.cs
@@ -0,0 +1,21 @@
+﻿/********************************************************************
+ * Develop by Jimmy Hu												*
+ * This program is licensed under the Apache License 2.0.			*
+ * WorkingMode.cs													*
+ * 本檔案撰寫WorkingMode類別以紀錄程式執行模式						*
+ ********************************************************************
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartOscilloscope                                                      //	命名空間為本程式
+{                                                                               //	進入UartOscilloscope命名空間
+	class WorkingMode                                                           //	WorkingMode類別
+	{                                                                           //	進入WorkingMode類別
+		private static int Program_Work_Mode = 0;								//	宣告Program_Work_Mode靜態變數，控制程式執行模式
+
+	}                                                                           //	結束WorkingMode類別
+}                                                                               //	結束UartOscilloscope命名空間
