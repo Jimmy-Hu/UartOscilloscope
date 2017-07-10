cd8115fdaf48ad39a862311d6675e2b104ffb9b8
60071jimmy <60071jimmy@gmail.com>
2017-07-10 14:09:28 +0800

    提取UART連線相關方法
    提取UART連線相關方法

--- UartOscilloscope/CSharpFiles/UARTConnection.cs
+++ UartOscilloscope/CSharpFiles/UARTConnection.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+/*	
+ */
+using System.IO.Ports;															//	使用System.IO.Ports函式庫
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +11,29 @@ namespace UartOscilloscope                                                      
 {                                                                               //	進入命名空間
 	public class UARTConnection                                                 //	UARTConnection類別
 	{                                                                           //	進入UARTConnection類別
-		public static int BaudRate;                                             //	宣告BaudRate靜態全域變數，控制SerialPort連線鮑率
-		public static int Parity_num;                                           //	宣告Parity_num靜態全域變數，控制SerialPort串列埠之Parity同位位元設定
-		public static int DataBits_num;                                         //	宣告DataBits_num靜態全域變數，控制SerialPort串列埠之DataBits數值
-		public void list_SerialPort()                                           //	偵測並列出已連線SerialPort副程式
+		private static SerialPort Uart_comport;                                 //	宣告新的SerialPort通訊埠，名稱為Uart_comport
+		private static int BaudRate;                                            //	宣告BaudRate靜態私有變數，控制SerialPort連線鮑率
+		private static int ParitySetting;                                       //	宣告ParitySetting靜態私有變數，控制SerialPort串列埠之Parity同位位元設定
+		private static int DataBitsSetting;                                     //	宣告DataBitsSetting靜態私有變數，控制SerialPort串列埠之DataBits數值
+		private static int COM_Port_num;                                        //	宣告COM_Port_num私有靜態變數，記錄已連線的SerialPort數量
+		private static bool Uart_comport_connected;                             //	宣告Uart_comport_connected布林變數，表示Uart_comport連線狀態
+		public static void InitializeUARTConnectionSetting()                    //	InitializeUARTConnectionSetting方法，初始化UART連線參數
+		{                                                                       //	進入InitializeUARTConnectionSetting方法
+			BaudRate = 9600;                                                    //	預設BaudRate數值為9600
+			ParitySetting = 0;                                                  //	預設ParitySetting數值為0(無同位位元檢查)
+			DataBitsSetting = 8;                                                //	預設DataBitsSetting數值為8
+			COM_Port_num = 0;                                                   //	預設COM_Port_num為0
+			Uart_comport_connected = false;                                     //	預設Uart_comport_connected值為False
+		}                                                                       //	結束InitializeUARTConnectionSetting方法
+		public static void list_SerialPort()                                    //	偵測並列出已連線SerialPort副程式
 		{                                                                       //	進入list_SerialPort副程式
 			DebugVariables.Set_list_SerialPort_Runtimes();                      //	呼叫Set_list_SerialPort_Runtimes方法遞增list_SerialPort_Runtimes變數
 			string[] ports = SerialPort.GetPortNames();                         //	偵測已連線的SerialPort並儲存結果至陣列ports
-			comboBox1.Items.Clear();                                            //	清空下拉式選單所有項目
+			Form1.comboBox1.Items.Clear();                                      //	清空下拉式選單所有項目
 			if (ports.Length == 0)                                              //	若偵測不到任何已連接的SerialPort(ports.Length為0)
 			{                                                                   //	進入if敘述
-				ErrorCode = 010001;                                             //	記錄ErrorCode
-				ErrorCodeMessage.Error_Message_Show(ErrorCode);                 //	顯示錯誤訊息
+				ErrorCodeMessage.Error_Message_Show((int)ErrorCodeMessage.ErrorCodeEncoding.NoSerialPortConnected);
+				//	顯示錯誤訊息
 				button2.Enabled = false;                                        //	關閉"連線/中斷連線"按鈕功能
 				textBox1.Enabled = false;                                       //	關閉textBox1(接收字串資料文字方塊)功能
 				return;                                                         //	提早結束list_SerialPort副程式
@@ -37,7 +51,7 @@ namespace UartOscilloscope                                                      
 			}                                                                   //	結束else敘述
 		}                                                                       //	結束list_SerialPort副程式
 		public void Uart_comport_handle                                         //	串列埠連線處理Uart_comport_handle副程式
-		(string comport_name, int Baud_Rate, int Parity_set)
+		(string comport_name)
 		//  處理Uart_comport連線設定
 		//  呼叫格式為Uart_comport_handle(comport名稱,連線鮑率,同位位元設定,)
 		//  同位位元設定說明：0為不檢查(None),1為奇同位檢察,2為偶同位檢察,3為同位位元恆為1,4為同位位元恆為0
@@ -79,23 +93,23 @@ namespace UartOscilloscope                                                      
 						button2.Enabled = true;                                 //	重新開啟"連線/中斷連線"按鈕功能
 						return;                                                 //	結束Uart_comport_handle副程式
 					}                                                           //	結束catch敘述
-					Uart_comport.BaudRate = Baud_Rate;                          //	設定Uart_comport連線之BaudRate
-					Uart_comport.DataBits = DataBits_num;                       //	設定Uart_comport連線之DataBits
-					switch (Parity_set)                                         //	根據Parity_set變數進行同位位元設定
+					Uart_comport.BaudRate = UARTConnection.BaudRate;            //	設定Uart_comport連線之BaudRate
+					Uart_comport.DataBits = DataBitsSetting;                    //	設定Uart_comport連線之DataBits
+					switch (UARTConnection.ParitySetting)                       //	根據UARTConnection.ParitySetting變數進行同位位元設定
 					{                                                           //	進入switch-case敘述
-						case 0:                                                 //	若為case0(Parity_set為0)
+						case 0:                                                 //	若為case0(UARTConnection.ParitySetting為0)
 							Uart_comport.Parity = Parity.None;                  //	無同位元檢查
 							break;                                              //	結束case0敘述、跳出switch-case
-						case 1:                                                 //	若為case1(Parity_set為1)
+						case 1:                                                 //	若為case1(UARTConnection.ParitySetting為1)
 							Uart_comport.Parity = Parity.Odd;                   //	奇同位位元檢查
 							break;                                              //	結束case1敘述、跳出switch-case
-						case 2:                                                 //	若為case2(Parity_set為2)
+						case 2:                                                 //	若為case2(UARTConnection.ParitySetting為2)
 							Uart_comport.Parity = Parity.Even;                  //	偶同位位元檢查
 							break;                                              //	結束case2敘述、跳出switch-case
-						case 3:                                                 //	若為case3(Parity_set為3)
+						case 3:                                                 //	若為case3(UARTConnection.ParitySetting為3)
 							Uart_comport.Parity = Parity.Mark;                  //	將同位位元恆設為1
 							break;                                              //	結束case3敘述、跳出switch-case
-						case 4:                                                 //	若為case4(Parity_set為4)
+						case 4:                                                 //	若為case4(UARTConnection.ParitySetting為4)
 							Uart_comport.Parity = Parity.Space;                 //	將同位位元恆設為0
 							break;                                              //	結束case4敘述、跳出switch-case
 					}                                                           //	結束switch-case敘述
@@ -109,7 +123,7 @@ namespace UartOscilloscope                                                      
 							button2.Text = "中斷連線";                          //	更改button2文字為"中斷連線"
 							button2.Enabled = true;                             //	重新開啟"連線/中斷連線"按鈕功能
 						}                                                       //	結束if敘述
-						Uart_comport.DataReceived += new SerialDataReceivedEventHandler(comport_DataReceived);
+						Uart_comport.DataReceived += new SerialDataReceivedEventHandler(Form1.comport_DataReceived);
 						//  處理資料接收
 					}                                                           //  結束try敘述
 					catch (Exception ex)                                        //  若發生錯誤狀況
