8daece481ba53486a0b0939276c5e32401e87dc5
60071jimmy <60071jimmy@gmail.com>
2017-07-10 09:59:47 +0800

    建立程式執行次數計數方法
    建立程式執行次數計數方法(Debug用)

--- UartOscilloscope/CSharpFiles/DebugVariables.cs
+++ UartOscilloscope/CSharpFiles/DebugVariables.cs
@@ -6,20 +6,102 @@ using System.Threading.Tasks;
 
 namespace UartOscilloscope                                                      //	命名空間為UartOscilloscope
 {                                                                               //	進入命名空間
-	class DebugVariables                                                        //  DebugVariables類別
+	public class DebugVariables                                                 //  DebugVariables類別
 	{                                                                           //  進入DebugVariables類別
-		private static uint button1_Click_Runtimes = 0;                         //	宣告button1_Click_Runtimes全域靜態變數，記錄button1_Click副程式執行次數，並初始化為0
+		private static uint button1_Click_Runtimes = 0;                         //	宣告button1_Click_Runtimes私有靜態變數，記錄button1_Click副程式執行次數，並初始化為0
+		public static void Set_button1_Click_Runtimes()                         //	Set_button1_Click_Runtimes方法
+		{                                                                       //	進入Set_button1_Click_Runtimes方法
+			button1_Click_Runtimes = button1_Click_Runtimes + 1;                //	遞增button1_Click_Runtimes變數
+		}                                                                       //	結束Set_button1_Click_Runtimes方法
+		public static uint Get_button1_Click_Runtimes()                         //	Get_button1_Click_Runtimes方法
+		{                                                                       //	進入Get_button1_Click_Runtimes方法
+			return button1_Click_Runtimes;                                      //	取得button1_Click_Runtimes變數數值
+		}                                                                       //	結束Get_button1_Click_Runtimes方法
 
-		public static uint button2_Click_Runtimes = 0;                          //	宣告button2_Click_Runtimes全域靜態變數，記錄button2_Click副程式執行次數，並初始化為0
-		public static uint button3_Click_Runtimes = 0;                          //	宣告button3_Click_Runtimes全域靜態變數，記錄button3_Click副程式執行次數，並初始化為0
-		public static uint Transmission_Setting_Click_Runtimes = 0;
-		//	宣告Transmission_Setting_Click_Runtimes全域靜態變數，記錄設定_傳輸設定ToolStripMenuItem_Click副程式執行次數，並初始化為0
-		public static uint User_Interface_Setting_Click_Runtimes = 0;
-		//	宣告User_Interface_Setting_Click_Runtimes全域靜態變數，記錄設定_介面設定ToolStripMenuItem_Click副程式執行次數，並初始化為0
-		public static uint list_SerialPort_Runtimes = 0;                        //	宣告list_SerialPort_Runtimes全域靜態變數，記錄list_SerialPort副程式執行次數
-		public static uint Uart_comport_handle_Runtimes = 0;                    //	宣告Uart_comport_handle_Runtimes全域靜態變數，記錄Uart_comport_handle副程式執行次數
-		public static uint comport_DataReceived_Runtimes = 0;                   //	宣告comport_DataReceived_Runtimes全域靜態變數，記錄comport_DataReceived副程式執行次數
-		public static uint DisplayText_Runtimes = 0;                            //	宣告DisplayText_Runtimes全域靜態變數，記錄DisplayText副程式執行次數
+		private static uint button2_Click_Runtimes = 0;                         //	宣告button2_Click_Runtimes私有靜態變數，記錄button2_Click副程式執行次數，並初始化為0
+		public static void Set_button2_Click_Runtimes()                         //	Set_button2_Click_Runtimes方法
+		{                                                                       //	進入Set_button2_Click_Runtimes方法
+			button2_Click_Runtimes = button2_Click_Runtimes + 1;                //	遞增button2_Click_Runtimes變數
+		}                                                                       //	結束Set_button2_Click_Runtimes方法
+		public static uint Get_button2_Click_Runtimes()                         //	Get_button2_Click_Runtimes方法
+		{                                                                       //	進入Get_button2_Click_Runtimes方法
+			return button2_Click_Runtimes;                                      //	取得button2_Click_Runtimes變數數值
+		}                                                                       //	結束Get_button2_Click_Runtimes方法
+
+		private static uint button3_Click_Runtimes = 0;                         //	宣告button3_Click_Runtimes私有靜態變數，記錄button3_Click副程式執行次數，並初始化為0
+		public static void Set_button3_Click_Runtimes()                         //	Set_button3_Click_Runtimes方法
+		{                                                                       //	進入Set_button3_Click_Runtimes方法
+			button3_Click_Runtimes = button3_Click_Runtimes + 1;                //	遞增button3_Click_Runtimes變數
+		}                                                                       //	結束Set_button3_Click_Runtimes方法
+		public static uint Get_button3_Click_Runtimes()                         //	Get_button3_Click_Runtimes方法
+		{                                                                       //	進入Get_button3_Click_Runtimes方法
+			return button3_Click_Runtimes;                                      //	取得button3_Click_Runtimes變數數值
+		}                                                                       //	結束Get_button3_Click_Runtimes方法
+
+		private static uint Transmission_Setting_Click_Runtimes = 0;
+		//	宣告Transmission_Setting_Click_Runtimes私有靜態變數，記錄設定_傳輸設定ToolStripMenuItem_Click副程式執行次數，並初始化為0
+		public static void Set_Transmission_Setting_Click_Runtimes()            //	Set_Transmission_Setting_Click_Runtimes方法
+		{                                                                       //	進入Set_Transmission_Setting_Click_Runtimes方法
+			Transmission_Setting_Click_Runtimes = Transmission_Setting_Click_Runtimes + 1;
+			//	遞增Transmission_Setting_Click_Runtimes變數
+		}                                                                       //	結束Set_Transmission_Setting_Click_Runtimes方法
+		public static uint Get_Transmission_Setting_Click_Runtimes()            //	Get_Transmission_Setting_Click_Runtimes方法
+		{                                                                       //	進入Get_Transmission_Setting_Click_Runtimes方法
+			return Transmission_Setting_Click_Runtimes;                         //	取得Transmission_Setting_Click_Runtimes變數數值
+		}                                                                       //	結束Get_Transmission_Setting_Click_Runtimes方法
+
+		private static uint User_Interface_Setting_Click_Runtimes = 0;
+		//	宣告User_Interface_Setting_Click_Runtimes私有靜態變數，記錄設定_介面設定ToolStripMenuItem_Click副程式執行次數，並初始化為0
+		public static void Set_User_Interface_Setting_Click_Runtimes()          //	Set_User_Interface_Setting_Click_Runtimes方法
+		{                                                                       //	進入Set_User_Interface_Setting_Click_Runtimes方法
+			User_Interface_Setting_Click_Runtimes = User_Interface_Setting_Click_Runtimes + 1;
+			//	遞增User_Interface_Setting_Click_Runtimes變數
+		}                                                                       //	結束Set_User_Interface_Setting_Click_Runtimes方法
+		public static uint Get_User_Interface_Setting_Click_Runtimes()          //	Get_User_Interface_Setting_Click_Runtimes方法
+		{                                                                       //	進入Get_User_Interface_Setting_Click_Runtimes方法
+			return User_Interface_Setting_Click_Runtimes;                       //	取得User_Interface_Setting_Click_Runtimes變數數值
+		}                                                                       //	結束Get_User_Interface_Setting_Click_Runtimes方法
+
+		private static uint list_SerialPort_Runtimes = 0;                       //	宣告list_SerialPort_Runtimes私有靜態變數，記錄list_SerialPort副程式執行次數
+		public static void Set_list_SerialPort_Runtimes()                       //	Set_list_SerialPort_Runtimes方法
+		{                                                                       //	進入Set_list_SerialPort_Runtimes方法
+			list_SerialPort_Runtimes = list_SerialPort_Runtimes + 1;            //	遞增list_SerialPort_Runtimes變數
+		}                                                                       //	結束Set_list_SerialPort_Runtimes方法
+		public static uint Get_list_SerialPort_Runtimes()                       //	Get_list_SerialPort_Runtimes方法
+		{                                                                       //	進入Get_list_SerialPort_Runtimes方法
+			return list_SerialPort_Runtimes;                                    //	取得list_SerialPort_Runtimes變數數值
+		}                                                                       //	結束Get_list_SerialPort_Runtimes方法
+
+		private static uint Uart_comport_handle_Runtimes = 0;                   //	宣告Uart_comport_handle_Runtimes私有靜態變數，記錄Uart_comport_handle副程式執行次數
+		public static void Set_Uart_comport_handle_Runtimes()                   //	Set_Uart_comport_handle_Runtimes方法
+		{                                                                       //	進入Set_Uart_comport_handle_Runtimes方法
+			Uart_comport_handle_Runtimes = Uart_comport_handle_Runtimes + 1;    //	遞增Uart_comport_handle_Runtimes變數
+		}                                                                       //	結束Set_Uart_comport_handle_Runtimes方法
+		public static uint Get_Uart_comport_handle_Runtimes()                   //	Get_Uart_comport_handle_Runtimes方法
+		{                                                                       //	進入Get_Uart_comport_handle_Runtimes方法
+			return Uart_comport_handle_Runtimes;                                //	取得Uart_comport_handle_Runtimes變數數值
+		}                                                                       //	結束Get_Uart_comport_handle_Runtimes方法
+
+		private static uint comport_DataReceived_Runtimes = 0;                  //	宣告comport_DataReceived_Runtimes私有靜態變數，記錄comport_DataReceived副程式執行次數
+		public static void Set_comport_DataReceived_Runtimes()                  //	Set_comport_DataReceived_Runtimes方法
+		{                                                                       //	進入Set_comport_DataReceived_Runtimes方法
+			comport_DataReceived_Runtimes = comport_DataReceived_Runtimes + 1;  //	遞增comport_DataReceived_Runtimes變數
+		}                                                                       //	結束Set_comport_DataReceived_Runtimes方法
+		public static uint Get_comport_DataReceived_Runtimes()                  //	Get_comport_DataReceived_Runtimes方法
+		{                                                                       //	進入Get_comport_DataReceived_Runtimes方法
+			return comport_DataReceived_Runtimes;                               //	取得comport_DataReceived_Runtimes變數數值
+		}                                                                       //	結束Get_comport_DataReceived_Runtimes方法
+
+		private static uint DisplayText_Runtimes = 0;                           //	宣告DisplayText_Runtimes私有靜態變數，記錄DisplayText副程式執行次數
+		public static void Set_DisplayText_Runtimes()                           //	Set_DisplayText_Runtimes方法
+		{                                                                       //	進入Set_DisplayText_Runtimes方法
+			DisplayText_Runtimes = DisplayText_Runtimes + 1;                    //	遞增DisplayText_Runtimes變數
+		}                                                                       //	結束Set_DisplayText_Runtimes方法
+		public static uint Get_DisplayText_Runtimes()                           //	Get_DisplayText_Runtimes方法
+		{                                                                       //	進入Get_DisplayText_Runtimes方法
+			return DisplayText_Runtimes;                                        //	取得DisplayText_Runtimes變數數值
+		}                                                                       //	結束Get_DisplayText_Runtimes方法
+
 	}                                                                           //  結束DebugVariables類別
 
 }																				//	結束命名空間
