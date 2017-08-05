2df4e3575fab5523b413c9657f94424d09a2e9bb
60071jimmy <60071jimmy@gmail.com>
2017-08-05 23:00:15 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/UARTConnection.cs
+++ UartOscilloscope/CSharpFiles/UARTConnection.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-/*	
+/*	串列埠(Comport)物件宣告於System.IO.Ports函式庫中
  */
 using System.IO.Ports;															//	使用System.IO.Ports函式庫
 using System.Linq;
