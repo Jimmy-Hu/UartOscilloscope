6a1ecb4fe0092ae6e01da087e13d952ab5a9442a
60071jimmy <60071jimmy@gmail.com>
2017-08-06 07:39:54 +0800

    寫入存取方法
    寫入存取方法

--- UartOscilloscope/CSharpFiles/UARTConnectionConstVal.cs
+++ UartOscilloscope/CSharpFiles/UARTConnectionConstVal.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 /*	串列埠(Comport)物件宣告於System.IO.Ports函式庫中
  */
@@ -18,5 +16,14 @@ namespace UartOscilloscope
 		private const int DefaultBaudRate = 9600;                               //	宣告DefaultBaudRate(預設鮑率)常數
 		/*	同位位元設定說明：0為不檢查(None),1為奇同位檢察,2為偶同位檢察,3為同位位元恆為1,4為同位位元恆為0 */
 		private const Parity DefaultParitySetting = 0;                          //	宣告DefaultParitySetting(預設同位位元設定)常數
+		private const int DefaultDataBitsSetting = 8;                           //	宣告DefaultDataBitsSetting(預設UART傳輸每組資料位元數)
+		public static int GetDefaultBaudRate()                                  //	GetDefaultBaudRate方法
+		{                                                                       //	進入GetDefaultBaudRate方法
+			return DefaultBaudRate;                                             //	回傳DefaultBaudRate變數
+		}                                                                       //	結束GetDefaultBaudRate方法
+		public static Parity GetDefaultParitySetting()                          //	GetDefaultParitySetting方法
+		{                                                                       //	進入GetDefaultParitySetting方法
+			return DefaultParitySetting;                                        //	回傳DefaultParitySetting變數
+		}                                                                       //	結束GetDefaultParitySetting方法
 	}                                                                           //	結束UARTConnectionConstVal類別
 }
