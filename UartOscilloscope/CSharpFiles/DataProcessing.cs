4640406d66c26e750f14db8b23cbb8207ec054ce
60071jimmy <60071jimmy@gmail.com>
2017-08-09 19:01:23 +0800

    撰寫String2CharArray方法
    撰寫String2CharArray方法

--- UartOscilloscope/CSharpFiles/DataProcessing.cs
+++ UartOscilloscope/CSharpFiles/DataProcessing.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;                                                     //	使用System.Windows.Forms函式庫
 
-namespace UartOscilloscope
-{
-	public class DataProcessing
-	{
-		public char[] String2CharArray(string InputData)
-		{
-			return InputData.ToCharArray();
-		}
-	}
-}
+namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
+{                                                                               //	進入命名空間
+	public class DataProcessing                                                 //	DataProcessing類別
+	{                                                                           //	進入DataProcessing類別
+		public char[] String2CharArray(string InputData)                        //	String2CharArray方法
+		{                                                                       //	進入String2CharArray方法
+			char[] OutputData = InputData.ToCharArray();                        //	宣告OutputData
+			foreach (char C in OutputData)
+			{
+				MessageBox.Show(C.ToString());
+			}
+			return OutputData;													//	回傳轉換結果
+		}                                                                       //	結束String2CharArray方法
+	}                                                                           //	結束DataProcessing類別
+}                                                                               //	結束命名空間
