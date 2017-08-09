a19991389c8afb58b95f3bc06d594dcc1bba5531
60071jimmy <60071jimmy@gmail.com>
2017-08-09 17:25:17 +0800

    新增註解
    新增註解

--- UartOscilloscopeTests/CSharpFiles/UARTConnectionConstValTests.cs
+++ UartOscilloscopeTests/CSharpFiles/UARTConnectionConstValTests.cs
@@ -9,15 +9,15 @@ using System.Threading.Tasks;
 namespace UartOscilloscope.Tests
 {                                                                               //	進入命名空間
 	[TestClass()]
-	public class UARTConnectionConstValTests
-	{
+	public class UARTConnectionConstValTests                                    //	UARTConnectionConstValTests類別
+	{                                                                           //	進入UARTConnectionConstValTests類別
 		[TestMethod()]
-		public void GetDefaultBaudRateTest()
-		{
-			int expected = 9600;
-			int actual;
-			actual = UARTConnectionConstVal.GetDefaultBaudRate();
-			Assert.AreEqual(expected, actual);
-		}
-	}
+		public void GetDefaultBaudRateTest()                                    //	GetDefaultBaudRateTest方法
+		{                                                                       //	進入GetDefaultBaudRateTest方法
+			int expected = 9600;                                                //	宣告expected(期望輸出)
+			int actual;                                                         //	宣告actual(實際輸出)
+			actual = UARTConnectionConstVal.GetDefaultBaudRate();               //	執行GetDefaultBaudRate方法
+			Assert.AreEqual(expected, actual);									//	比對期望輸出與實際輸出
+		}                                                                       //	結束GetDefaultBaudRateTest方法
+	}                                                                           //	結束UARTConnectionConstValTests類別
 }                                                                               //	結束命名空間
