21709480d273f1c8c13f17b2834136ab7b567f2d
60071jimmy <60071jimmy@gmail.com>
2017-08-09 17:42:54 +0800

    新增註解
    新增註解

--- UartOscilloscopeTests/CSharpFiles/DataProcessingTests.cs
+++ UartOscilloscopeTests/CSharpFiles/DataProcessingTests.cs
@@ -6,15 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UartOscilloscope.Tests
-{
+namespace UartOscilloscope.Tests                                                //	UartOscilloscope.Tests命名空間
+{                                                                               //	進入命名空間
 	[TestClass()]
-	public class DataProcessingTests
-	{
+	public class DataProcessingTests                                            //	DataProcessingTests類別
+	{                                                                           //	進入DataProcessingTests類別
 		[TestMethod()]
 		public void String2CharArrayTest()
 		{
 			Assert.Fail();
 		}
-	}
-}
+	}                                                                           //	結束DataProcessingTests類別
+}                                                                               //	結束命名空間
