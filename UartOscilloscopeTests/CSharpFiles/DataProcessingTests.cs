f5fdd6b502c0a0975763b50a8991f8dcc0255eb3
60071jimmy <60071jimmy@gmail.com>
2017-08-09 17:38:19 +0800

    建立DataProcessingTests類別
    建立DataProcessingTests類別

---
+++ UartOscilloscopeTests/CSharpFiles/DataProcessingTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UartOscilloscope;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartOscilloscope.Tests
+{
+	[TestClass()]
+	public class DataProcessingTests
+	{
+		[TestMethod()]
+		public void String2CharArrayTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
