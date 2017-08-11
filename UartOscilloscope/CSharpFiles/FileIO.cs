786df6958a915961c391eda5992138474b9aafde
60071jimmy <60071jimmy@gmail.com>
2017-08-11 11:20:17 +0800

    修改註解
    修改註解

--- UartOscilloscope/CSharpFiles/FileIO.cs
+++ UartOscilloscope/CSharpFiles/FileIO.cs
@@ -16,10 +16,10 @@ namespace UartOscilloscope
 	public class FileIO                                                         //	FileIO類別
 	{                                                                           //	進入FileIO類別
 		public void FileWrite(string File_name, string Input_string)
-		//  宣告FileWrite副程式，將資料寫入檔案
+		//  宣告FileWrite方法，將資料寫入檔案
 		//  File_name為欲寫入檔案名稱
 		//  Input_string為欲寫入檔案之字串資料
-		{                                                                       //  進入FileWrite副程式
+		{                                                                       //  進入FileWrite方法
 			FileStream file_stream = new FileStream(File_name, FileMode.Append);
 			//  建立檔案指標，指向指定檔案名稱，模式為傳入之File_mode
 			byte[] Input_data = System.Text.Encoding.Default.GetBytes(Input_string);
@@ -27,19 +27,19 @@ namespace UartOscilloscope
 			file_stream.Write(Input_data, 0, Input_data.Length);                //  寫入資料至檔案中
 			file_stream.Flush();                                                //  清除緩衝區
 			file_stream.Close();                                                //  關閉檔案
-		}                                                                       //  結束FileWrite副程式
+		}                                                                       //  結束FileWrite方法
 		public void FileWrite(string File_name, string Input_string, FileMode File_mode)
-		//  宣告FileWrite副程式，將資料寫入檔案
+		//  宣告FileWrite方法，將資料寫入檔案
 		//  File_name為欲寫入檔案名稱
 		//  Input_string為欲寫入檔案之字串資料
 		//  File_mode為開啟檔案模式
-		{                                                                       //  進入FileWrite副程式
+		{                                                                       //  進入FileWrite方法
 			FileStream file_stream = new FileStream(File_name, File_mode);      //  建立檔案指標，指向指定檔案名稱，模式為傳入之File_mode
 			byte[] Input_data = System.Text.Encoding.Default.GetBytes(Input_string);
 			//  將填入資料轉為位元陣列
 			file_stream.Write(Input_data, 0, Input_data.Length);                //  寫入資料至檔案中
 			file_stream.Flush();                                                //  清除緩衝區
 			file_stream.Close();                                                //  關閉檔案
-		}                                                                       //  結束FileWrite副程式
+		}                                                                       //  結束FileWrite方法
 	}                                                                           //	結束FileIO類別
 }                                                                               //	結束命名空間
