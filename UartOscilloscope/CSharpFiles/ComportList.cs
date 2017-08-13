53b1923b963bfff9b1b5795ac280d24eef615699
60071jimmy <60071jimmy@gmail.com>
2017-08-13 09:41:27 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/ComportList.cs
+++ UartOscilloscope/CSharpFiles/ComportList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
 {                                                                               //	進入UartOscilloscope命名空間
-	class ComportList
-	{
+	class ComportList                                                           //	ComportList類別
+	{                                                                           //	進入ComportList類別
 	}                                                                           //	結束ComportList類別
 }                                                                               //	結束UartOscilloscope命名空間
