4087e37b7bf4adedf13c9100b9e35e4b2e65a480
60071jimmy <60071jimmy@gmail.com>
2017-08-13 09:36:30 +0800

    建立ComportList類別
    建立ComportList類別

---
+++ UartOscilloscope/CSharpFiles/ComportList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
+{                                                                               //	進入UartOscilloscope命名空間
+	class ComportList
+	{
+	}
+}
