5c6e8b23f21988ac81d2fe6ffaaf17a6c3bddf4a
60071jimmy <60071jimmy@gmail.com>
2017-08-17 11:47:17 +0800

    SetComportList更名
    SetComportList更名為UpdateComportList

--- UartOscilloscope/CSharpFiles/ComportList.cs
+++ UartOscilloscope/CSharpFiles/ComportList.cs
@@ -20,10 +20,10 @@ namespace UartOscilloscope                                                      
 		{                                                                       //	進入GetComportList方法
 			return Name;                                                        //	回傳Name
 		}                                                                       //	結束GetComportList方法
-		public void SetComportList(string[] ComportList)                        //	SetComportList方法
-		{                                                                       //	進入SetComportList方法
+		public void UpdateComportList(string[] ComportList)                     //	UpdateComportList方法
+		{                                                                       //	進入UpdateComportList方法
 			Name = ComportList;													//	填入資料
-		}                                                                       //	結束SetComportList方法
+		}                                                                       //	結束UpdateComportList方法
 
 	}                                                                           //	結束ComportList類別
 }                                                                               //	結束UartOscilloscope命名空間
