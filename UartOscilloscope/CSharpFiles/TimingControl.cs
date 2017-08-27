327ef8c47537ed740ea8ef840aa2b8c1e30bb79d
60071jimmy <60071jimmy@gmail.com>
2017-08-27 12:07:44 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/TimingControl.cs
+++ UartOscilloscope/CSharpFiles/TimingControl.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UartOscilloscope.CSharpFiles
-{
-	class TimingControl
-	{
-	}
-}
+namespace UartOscilloscope                                                      //	命名空間為UartOscilloscope
+{                                                                               //	進入UartOscilloscope命名空間
+	class TimingControl                                                         //	TimingControl類別
+	{                                                                           //	進入TimingControl類別
+	}                                                                           //	結束TimingControl類別
+}                                                                               //	結束UartOscilloscope命名空間
