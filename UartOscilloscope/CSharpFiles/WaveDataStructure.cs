c1ab766e9e87a0e655d06990fd1addf7344f11fd
jimmy hu <60071jimmy@gmail.com>
2017-09-01 06:24:58 +0800

    建立WaveDataStructure建構子
    建立WaveDataStructure建構子

--- UartOscilloscope/CSharpFiles/WaveDataStructure.cs
+++ UartOscilloscope/CSharpFiles/WaveDataStructure.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
 {                                                                               //	進入命名空間
-	public class WaveDataStructure                                                     //	WaveDataStructure類別
+	public class WaveDataStructure                                              //	WaveDataStructure類別
 	{                                                                           //	進入WaveDataStructure類別
-		public static int[] WaveRawData;
+		public int[] WaveRawData;
+		public WaveDataStructure()
+		{
 
+		}
 	}                                                                           //	結束WaveDataStructure類別
 }																				//	結束命名空間
