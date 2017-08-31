1d4c336de1c3e4787a8929523975694f402d0de4
jimmy hu <60071jimmy@gmail.com>
2017-08-31 23:32:50 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/WaveDataStructure.cs
+++ UartOscilloscope/CSharpFiles/WaveDataStructure.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UartOscilloscope.CSharpFiles
-{
-	class WaveDataStructure
-	{
-	}
-}
+namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
+{                                                                               //	進入命名空間
+	class WaveDataStructure                                                     //	WaveDataStructure類別
+	{                                                                           //	進入WaveDataStructure類別
+
+	}                                                                           //	結束WaveDataStructure類別
+}																				//	結束命名空間
