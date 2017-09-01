1bb858a71e3c3381d1ffcf14ef3d59c6958dfcf1
jimmy hu <60071jimmy@gmail.com>
2017-09-01 10:43:26 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/WaveDataStructure.cs
+++ UartOscilloscope/CSharpFiles/WaveDataStructure.cs
@@ -8,15 +8,19 @@ namespace UartOscilloscope                                                      
 {                                                                               //	進入命名空間
 	public class WaveDataStructure                                              //	WaveDataStructure類別
 	{                                                                           //	進入WaveDataStructure類別
+		/// <summary>
+		/// WaveRawData陣列用於儲存感測通道資料，資料以環型方式寫入更新
+		/// </summary>
 		public int[] WaveRawData;                                               //	宣告WaveRawData資料儲存陣列
-		private int NewDataIndex;
+		private int NewDataIndex;												//	宣告NewDataIndex，記錄下一筆資料儲存位址
 		/// <summary>
 		/// WaveDataStructure建構子，初始化資料陣列
 		/// </summary>
 		/// <param name="InitialSize">變數用於給定資料陣列初始大小</param>
 		public WaveDataStructure(int InitialSize)                               //	WaveDataStructure建構子
 		{                                                                       //	進入WaveDataStructure建構子
-			Array.Resize<int>(ref WaveRawData, InitialSize);
+			Array.Resize<int>(ref WaveRawData, InitialSize);					//	初始化陣列大小
 		}                                                                       //	結束WaveDataStructure建構子
+
 	}                                                                           //	結束WaveDataStructure類別
 }																				//	結束命名空間
