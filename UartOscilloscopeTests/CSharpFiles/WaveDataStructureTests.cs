f8f5ebbf06a8a9c6a08b8d181591e41e0b10e370
jimmy hu <60071jimmy@gmail.com>
2017-09-02 13:14:51 +0800

    建立WaveDataStructureInitialTest方法
    建立WaveDataStructureInitialTest方法

--- UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
+++ UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
@@ -18,9 +18,20 @@ namespace UartOscilloscope.Tests                                                
 			int ArrayMax = 3;                                                   //	宣告ArrayMax變數，用於給定WaveDataStructureTest物件空間
 			int[] TestData = new int[]{ 3, 2, 1 };								//	建立測試資料
 			WaveDataStructureTest1 = new WaveDataStructure(ArrayMax);           //	初始化測試物件
+
 			WaveDataStructureTest1.AddData(3);
+
 			WaveDataStructureTest1.AddData(2);
 
 		}                                                                       //	結束AddDataTest方法
+		/// <summary>
+		/// WaveDataStructureInitialTest方法用於測試WaveDataStructure初始化功能
+		/// 若順利初始化，則內部陣列空間資料皆為0
+		/// </summary>
+		/// <returns></returns>
+		private bool WaveDataStructureInitialTest()                             //	WaveDataStructureInitialTest方法
+		{                                                                       //	進入WaveDataStructureInitialTest方法
+
+		}                                                                       //	結束WaveDataStructureInitialTest方法
 	}                                                                           //	結束WaveDataStructureTests類別
 }                                                                               //	結束命名空間
