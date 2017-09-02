a15a96dcb3609958224a8c64708a1a59a277342d
jimmy hu <60071jimmy@gmail.com>
2017-09-02 12:49:21 +0800

    新增註解
    新增註解

--- UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
+++ UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;                             //	使用Microsoft.VisualStudio.TestTools.UnitTesting函式庫
+using UartOscilloscope;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UartOscilloscopeTests.CSharpFiles
-{
-	class WaveDataStructureTests
-	{
-	}
-}
+namespace UartOscilloscope.Tests                                                //	UartOscilloscope.Tests命名空間
+{                                                                               //	進入命名空間
+	[TestClass()]
+	public class WaveDataStructureTests                                         //	WaveDataStructureTests類別
+	{                                                                           //	進入WaveDataStructureTests類別
+		[TestMethod()]
+		public void AddDataTest()
+		{
+			Assert.Fail();
+		}
+	}                                                                           //	結束WaveDataStructureTests類別
+}                                                                               //	結束命名空間
