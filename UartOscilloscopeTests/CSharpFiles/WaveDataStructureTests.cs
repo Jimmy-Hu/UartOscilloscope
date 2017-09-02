336a434cc8421177188ca33c069ad26608f75685
jimmy hu <60071jimmy@gmail.com>
2017-09-02 13:00:12 +0800

    撰寫AddDataTest方法
    撰寫AddDataTest方法

--- UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
+++ UartOscilloscopeTests/CSharpFiles/WaveDataStructureTests.cs
@@ -14,7 +14,10 @@ namespace UartOscilloscope.Tests                                                
 		[TestMethod()]
 		public void AddDataTest()                                               //	AddDataTest方法
 		{                                                                       //	進入AddDataTest方法
-			Assert.Fail();
+			WaveDataStructure WaveDataStructureTest1;                           //	宣告WaveDataStructureTest1物件
+			int ArrayMax = 3;                                                   //	宣告ArrayMax變數，用於給定WaveDataStructureTest物件空間
+			WaveDataStructureTest1 = new WaveDataStructure(ArrayMax);			//	初始化測試物件
+
 		}                                                                       //	結束AddDataTest方法
 	}                                                                           //	結束WaveDataStructureTests類別
 }                                                                               //	結束命名空間
