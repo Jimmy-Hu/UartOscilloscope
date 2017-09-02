a0bce43654df59d47d67d914ea76b13253d2459e
jimmy hu <60071jimmy@gmail.com>
2017-09-02 13:20:29 +0800

    建立TestingType類別
    建立TestingType類別

---
+++ UartOscilloscope/CSharpFiles/TestingType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartOscilloscope.Tests                                                //	UartOscilloscope.Tests命名空間
+{																				//	進入命名空間
+	/// <summary>
+	/// TestingType類別物件用於記錄測試結果
+	/// 若測試成功，
+	/// </summary>
+	class TestingType                                                           //	TestingType類別
+	{                                                                           //	進入TestingType類別
+		bool Result;
+		public TestingType()                                                    //	TestingType建構子
+		{                                                                       //	進入TestingType建構子
+			Result = false;
+		}                                                                       //	結束TestingType建構子
+
+	}                                                                           //	結束TestingType類別
+}																				//	結束命名空間
