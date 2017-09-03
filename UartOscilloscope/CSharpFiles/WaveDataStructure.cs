38bf5f0fb5e157d9b25e2b99b9c66f42ee7a1113
jimmy hu <60071jimmy@gmail.com>
2017-09-03 16:19:29 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/WaveDataStructure.cs
+++ UartOscilloscope/CSharpFiles/WaveDataStructure.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System;                                                                   //	使用System函式庫
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
