3b4b8e31ac07a34526d83116c558efa0398a0de7
jimmy hu <60071jimmy@gmail.com>
2017-09-19 23:58:37 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/CommandParser.cs
+++ UartOscilloscope/CSharpFiles/CommandParser.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace UartOscilloscope
-{
-	/// <summary>
-	/// CommandParser類別用於建立指令語法分析器
-	/// </summary>
-	class CommandParser
+namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
+{                                                                               //	進入UartOscilloscope命名空間
+																				/// <summary>
+																				/// CommandParser類別用於建立指令語法分析器
+																				/// </summary>
+	class CommandParser					
 	{
 	}
 }
