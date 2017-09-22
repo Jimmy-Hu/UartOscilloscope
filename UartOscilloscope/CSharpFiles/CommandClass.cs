3ff3ae55e34aedfe925340dd256547fe8568d2e1
jimmy hu <60071jimmy@gmail.com>
2017-09-22 16:37:58 +0800

    新增註解
    新增註解

--- UartOscilloscope/CSharpFiles/CommandClass.cs
+++ UartOscilloscope/CSharpFiles/CommandClass.cs
@@ -7,15 +7,15 @@ namespace UartOscilloscope                                                      
 {                                                                               //	進入UartOscilloscope命名空間
 	class CommandClass                                                          //	CommandClass指令類別
 	{                                                                           //	進入CommandClass指令類別
-		private int CommandID;														//	宣告CommandID為指令編號
-		private string Command;                                                     //	宣告Command為指令內容
+		private int CommandID;													//	宣告CommandID為指令編號
+		private string Command;                                                 //	宣告Command為指令內容
 		/// <summary>
 		/// CommandClass建構子
 		/// </summary>
 		/// <param name="CommandID"></param>
 		/// <param name="Command"></param>
-		public CommandClass(int CommandID, string Command)
-		{
+		public CommandClass(int CommandID, string Command)                      //	CommandClass建構子
+		{                                                                       //	進入CommandClass建構子
 			this.CommandID = CommandID;
 
 		}
