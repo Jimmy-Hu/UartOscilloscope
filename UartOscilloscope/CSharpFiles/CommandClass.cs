35a387e4c6dd9f3284c9dd67dd8f95174e610f64
jimmy hu <60071jimmy@gmail.com>
2017-09-22 14:15:10 +0800

    撰寫CommandClass指令類別
    撰寫CommandClass指令類別

--- UartOscilloscope/CSharpFiles/CommandClass.cs
+++ UartOscilloscope/CSharpFiles/CommandClass.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace UartOscilloscope                                                      //	UartOscilloscope命名空間
 {                                                                               //	進入UartOscilloscope命名空間
-	private int CommandID;														//	宣告CommandID為指令編號
-	private string Command;                                                     //	宣告Command為指令內容
 	class CommandClass                                                          //	CommandClass指令類別
 	{                                                                           //	進入CommandClass指令類別
+		private int CommandID;														//	宣告CommandID為指令編號
+		private string Command;                                                     //	宣告Command為指令內容
 		/// <summary>
 		/// CommandClass建構子
 		/// </summary>
@@ -18,6 +16,7 @@ namespace UartOscilloscope                                                      
 		/// <param name="Command"></param>
 		public CommandClass(int CommandID, string Command)
 		{
+			this.CommandID = CommandID;
 
 		}
 	}                                                                           //	結束CommandClass指令類別
