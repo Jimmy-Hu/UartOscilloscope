cbdce6fe1cdeccdd82be38a6d0631000a1222ed7
jimmy hu <60071jimmy@gmail.com>
2017-09-23 09:25:35 +0800

    Command更名
    Command更名為CommandName

--- UartOscilloscope/CSharpFiles/CommandClass.cs
+++ UartOscilloscope/CSharpFiles/CommandClass.cs
@@ -8,19 +8,19 @@ namespace UartOscilloscope                                                      
 {                                                                               //	進入UartOscilloscope命名空間
 	class CommandClass                                                          //	CommandClass指令類別
 	{                                                                           //	進入CommandClass指令類別
-		private int CommandID;													//	宣告CommandID為指令編號
-		private string Command;                                                 //	宣告Command為指令內容
+		private int CommandID;													//	宣告指令編號
+		private string CommandName;                                             //	宣告指令名稱
 		private Task task;														//	宣告指令工作
 		/// <summary>
 		/// CommandClass建構子
 		/// </summary>
 		/// <param name="CommandID">為指令編號</param>
-		/// <param name="Command">為指令內容</param>
+		/// <param name="CommandName">為指令名稱</param>
 		/// <param name="task">為指令工作</param>
-		public CommandClass(int CommandID, string Command, Task task)           //	CommandClass建構子
+		public CommandClass(int CommandID, string CommandName, Task task)       //	CommandClass建構子
 		{                                                                       //	進入CommandClass建構子
 			this.CommandID = CommandID;                                         //	初始化指令編號
-			this.Command = Command;                                             //	初始化指令內容
+			this.CommandName = CommandName;                                     //	初始化指令名稱
 			this.task = task;													//	初始化指令工作
 		}                                                                       //	結束CommandClass建構子
 	}                                                                           //	結束CommandClass指令類別
