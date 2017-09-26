682b25023b7c6257bfdf01058a09852f8f07e04b
jimmy hu <60071jimmy@gmail.com>
2017-09-26 12:33:33 +0800

    撰寫GetCommandSet方法
    撰寫GetCommandSet方法

--- UartOscilloscope/CSharpFiles/CommandDatabase.cs
+++ UartOscilloscope/CSharpFiles/CommandDatabase.cs
@@ -41,12 +41,15 @@ namespace UartOscilloscope                                                      
 		public string[] GetCommandSet()                                         //	GetCommandSet方法
 		{                                                                       //	進入GetCommandSet方法
 			string[] OutputStrArray;                                            //	宣告輸出字串陣列
-			OutputStrArray = new string[] { };									//	初始化字串陣列
-			Array.Resize<string>(ref OutputStrArray, CommandSet.Count);			//	調整字串陣列大小
-			for(int Index = 0; Index < CommandSet.Count; Index++)				//	以for迴圈依序將資料填入
-			{																	//	進入for迴圈
-
-			}																	//	結束for迴圈
+			OutputStrArray = new string[] { };                                  //	初始化字串陣列
+			int OutputStrArraySize = CommandSet.Count;                          //	宣告OutputStrArraySize，用於記錄輸出陣列大小
+			Array.Resize<string>(ref OutputStrArray, OutputStrArraySize);       //	調整字串陣列大小
+			int Index = 0;														//	宣告索引值用於存取陣列元素
+			foreach(CommandClass Item in this.CommandSet)						//	取出命令集合中指令
+			{																	//	進入foreach敘述
+				OutputStrArray[Index] = Item.ToString();						//	填入資料至輸出陣列
+				Index = Index + 1;												//	遞增索引值
+			}																	//	結束foreach敘述
 			return OutputStrArray;												//	回傳字串陣列
 		}                                                                       //	結束GetCommandSet方法
 	}                                                                           //	結束CommandDatabase類別
