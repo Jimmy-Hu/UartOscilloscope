cf71ca4c9b084a87a03ec7f55aa589cd34a16fc6
jimmy hu <60071jimmy@gmail.com>
2018-01-11 22:31:07 +0800

    Edit CommandClass class
    Edit CommandClass class

--- UartOscilloscope/CSharpFiles/CommandClass.cs
+++ UartOscilloscope/CSharpFiles/CommandClass.cs
@@ -10,6 +10,7 @@ namespace UartOscilloscope                                                      
 	{                                                                           //	進入CommandClass指令類別
 		private int CommandID;													//	宣告指令編號
 		private string CommandName;                                             //	宣告指令名稱
+		private string ChineseDescription;										//	宣告指令中文描述
 		private Task task;														//	宣告指令工作
 		/// <summary>
 		/// CommandClass建構子
@@ -17,10 +18,11 @@ namespace UartOscilloscope                                                      
 		/// <param name="CommandID">為指令編號</param>
 		/// <param name="CommandName">為指令名稱</param>
 		/// <param name="task">為指令工作</param>
-		public CommandClass(int CommandID, string CommandName, Task task)       //	CommandClass建構子
+		public CommandClass(int CommandID, string CommandName, string ChineseDescription, Task task)       //	CommandClass建構子
 		{                                                                       //	進入CommandClass建構子
 			this.CommandID = CommandID;                                         //	初始化指令編號
 			this.CommandName = CommandName;                                     //	初始化指令名稱
+			this.ChineseDescription = ChineseDescription;						//	初始化指令中文描述
 			this.task = task;													//	初始化指令工作
 		}                                                                       //	結束CommandClass建構子
 		/// <summary>
