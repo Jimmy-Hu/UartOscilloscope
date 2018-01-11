9b6ea72eb2d0c4724562e6d294aa01da34e8bad4
jimmy hu <60071jimmy@gmail.com>
2018-01-12 06:40:21 +0800

    Edit DataQueue.cs
    Edit DataQueue.cs

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/DataQueue.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/DataQueue.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/********************************************************************
+ * Develop by Jimmy Hu												*
+ * This program is licensed under the Apache License 2.0.			*
+ * DataQueue.cs														*
+ * 本檔案建立繪圖資料佇列單元物件									*
+ ********************************************************************
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +16,29 @@ namespace QueueDataGraphic.CSharpFiles
 {                                                                               //	namespace start
 	class DataQueue
 	{
+		/// <summary>
+		/// GraphicData is the queue for storing the graphic data.
+		/// GraphicData佇列用於儲存繪圖資料
+		/// </summary>
+		private Queue<object> GraphicData;
+
+		/// <summary>
+		/// GraphicDataQueueMax is the max number of count of GraphData elements.
+		/// GraphicDataQueueMax為GraphData物件數量上限
+		/// </summary>
+		private int GraphicDataQueueMax;
+
+		/// <summary>
+		/// RemovingOverload method would remove the data when GraphicData.count > GraphicDataQueueMax
+		/// RemovingOverload用於清除多於物件數量上限之資料
+		/// </summary>
+		public void RemovingOverload()                                          //	RemovingOverload method, RemovingOverload方法
+		{                                                                       //	RemovingOverload method start, 進入RemovingOverload方法
+			while(GraphicData.Count > GraphicDataQueueMax)                      //	when GraphicData.Count more than GraphicDataQueueMax, 當有過多資料
+			{																	//	while loop start, 進入while迴圈
+
+			}																	//	while loop end, 結束while迴圈
+		}                                                                       //	RemovingOverload method end, 結束RemovingOverload方法
 
 	}
 }                                                                               //	namespace end
