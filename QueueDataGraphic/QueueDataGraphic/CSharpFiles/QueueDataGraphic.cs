658f55b303c56bd75ba53708088bc7768a60154d
jimmy hu <60071jimmy@gmail.com>
2018-01-12 22:40:29 +0800

    Edit QueueDataGraphic.cs
    Edit QueueDataGraphic.cs

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -16,6 +16,6 @@ namespace QueueDataGraphic.CSharpFiles
 {                                                                               // namespace start, 進入命名空間
 	class QueueDataGraphic                                                      // QueueDataGraphic class, QueueDataGraphic類別
 	{                                                                           // QueueDataGraphic class start, 進入QueueDataGraphic類別
-
+		List<DataQueue> DataQueueList;
 	}                                                                           // QueueDataGraphic class end, 結束QueueDataGraphic類別
 }                                                                               // namespace end, 結束命名空間
