4f5227fcacccb4c01781275eb3309d55dbc539ff
jimmy hu <60071jimmy@gmail.com>
2018-01-12 23:49:06 +0800

    Edit QueueDataGraphic.cs
    Edit QueueDataGraphic.cs

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -17,18 +17,14 @@ namespace QueueDataGraphic.CSharpFiles
 	class QueueDataGraphic                                                      //	QueueDataGraphic class, QueueDataGraphic類別
 	{                                                                           //	QueueDataGraphic class start, 進入QueueDataGraphic類別
 		List<DataQueue> DataQueueList;                                          //	DataQueueList object, DataQueueList物件
-
-		/// <summary>
-		/// QueueDataGraphic constructor, QueueDataGraphic建構子
-		/// </summary>
-		/// <param name="ChannelCount">the number of graphic channels, 繪圖通道數量</param>
-		public QueueDataGraphic(int ChannelCount)                               //	QueueDataGraphic constructor, QueueDataGraphic建構子
+		
+		public QueueDataGraphic(List<string> DataQueueNames)                    //	QueueDataGraphic constructor, QueueDataGraphic建構子
 		{                                                                       //	QueueDataGraphic constructor start, 進入QueueDataGraphic建構子
 			DataQueueList = new List<DataQueue>();                              //	initialize DataQueueList, 初始化DataQueueList物件
-			for (int Loopnum = 0; Loopnum < ChannelCount; Loopnum++)			//	add graphic channels, 加入繪圖通道
-			{																	//	for loop start, 進入for迴圈
-				DataQueueList.Add(new DataQueue());                             //	add graphic channels, 加入繪圖通道
-			}																	//	for loop end, 結束for迴圈
+			foreach (string item in DataQueueNames)                             //	get each name of DataQueueNames
+			{                                                                   //	foreach statement start, 進入foreach敘述
+				DataQueueList.Add(new DataQueue(item));
+			}                                                                   //	foreach statement end, 結束foreach敘述
 		}                                                                       //	QueueDataGraphic constructor end, 結束QueueDataGraphic建構子
 
 	}                                                                           //	QueueDataGraphic class end, 結束QueueDataGraphic類別
