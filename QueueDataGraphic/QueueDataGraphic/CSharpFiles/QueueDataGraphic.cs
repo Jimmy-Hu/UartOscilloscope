58a49579ebee79d453e71d7bb72f8c55f04acaa3
jimmy hu <60071jimmy@gmail.com>
2018-01-13 00:53:05 +0800

    Edit AddData method
    Edit AddData method

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -31,15 +31,21 @@ namespace QueueDataGraphic.CSharpFiles
 			}                                                                   //	foreach statement end, 結束foreach敘述
 		}                                                                       //	QueueDataGraphic constructor end, 結束QueueDataGraphic建構子
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="DataQueueName">欲新增資料之Queue名稱</param>
+		/// <param name="InputData"></param>
 		public void AddData(string DataQueueName, object InputData)             //	AddData method, AddData方法
 		{                                                                       //	AddData method start, 進入AddData方法
 			foreach (DataQueue item in DataQueueList)                           //	search DataQueue in DataQueueList
 			{                                                                   //	foreach statement start, 進入foreach敘述
 				if (item.GetDataQueueName() == DataQueueName)                   //	if item name is as same as DataQueueName, 若搜尋得相同名稱
 				{                                                               //	if statement start, 進入if敘述
-
+					item.AddData(InputData);									//	add data to queue, 新增資料至對應佇列
 				}                                                               //	if statement end, 結束if敘述
 			}                                                                   //	foreach statement end, 結束foreach敘述
 		}                                                                       //	AddData method end, 結束AddData方法
+
 	}                                                                           //	QueueDataGraphic class end, 結束QueueDataGraphic類別
 }                                                                               //	namespace end, 結束命名空間
