b47f389a4669dd1e65183f88eb9171e03ef90ab6
jimmy hu <60071jimmy@gmail.com>
2018-01-13 00:51:27 +0800

    Edit AddData method
    Edit AddData method

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -33,10 +33,13 @@ namespace QueueDataGraphic.CSharpFiles
 
 		public void AddData(string DataQueueName, object InputData)             //	AddData method, AddData方法
 		{                                                                       //	AddData method start, 進入AddData方法
-			foreach (DataQueue item in DataQueueList)
-			{
+			foreach (DataQueue item in DataQueueList)                           //	search DataQueue in DataQueueList
+			{                                                                   //	foreach statement start, 進入foreach敘述
+				if (item.GetDataQueueName() == DataQueueName)                   //	if item name is as same as DataQueueName, 若搜尋得相同名稱
+				{                                                               //	if statement start, 進入if敘述
 
-			}
+				}                                                               //	if statement end, 結束if敘述
+			}                                                                   //	foreach statement end, 結束foreach敘述
 		}                                                                       //	AddData method end, 結束AddData方法
 	}                                                                           //	QueueDataGraphic class end, 結束QueueDataGraphic類別
 }                                                                               //	namespace end, 結束命名空間
