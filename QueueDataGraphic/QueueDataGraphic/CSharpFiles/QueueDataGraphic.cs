ce5b3e3c6d472670875a982b26819307cd0f9c13
jimmy hu <60071jimmy@gmail.com>
2018-01-12 21:52:14 +0800

    Add comment
    Add comment

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QueueDataGraphic.CSharpFiles
-{
+{                                                                               //	namespace start, 進入命名空間
 	class QueueDataGraphic                                                      // QueueDataGraphic class, QueueDataGraphic類別
 	{                                                                           // QueueDataGraphic class start, 進入QueueDataGraphic類別
 
 	}                                                                           // QueueDataGraphic class end, 結束QueueDataGraphic類別
-}
+}                                                                               //	namespace end, 結束命名空間
