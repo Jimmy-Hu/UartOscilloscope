38e88d10b60d87a7542e1744e0dcc39902a10c04
jimmy hu <60071jimmy@gmail.com>
2018-01-12 22:24:23 +0800

    Add comment
    Add comment

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/DataQueue.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/DataQueue.cs
@@ -28,11 +28,11 @@ namespace QueueDataGraphic.CSharpFiles
 		/// </summary>
 		private int GraphicDataQueueMax;
 
-		public DataQueue()
-		{
+		public DataQueue()                                                      //	DataQueue constructor, DataQueue建構子
+		{                                                                       //	DataQueue constructor start, 進入DataQueue建構子
 			GraphicData = new Queue<object>();                                  //	initialize GraphicData, 初始化GraphicData物件
 			GraphicDataQueueMax = 100;                                          //	initialize GraphicDataQueueMax, 初始化GraphicDataQueueMax變數
-		}
+		}                                                                       //	DataQueue constructor end, 結束DataQueue建構子
 
 		/// <summary>
 		/// SetGraphicDataQueueMax method would update GraphicDataQueueMax variable.
