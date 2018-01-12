6736af383071bc6a8edc21ed023357911cadb107
jimmy hu <60071jimmy@gmail.com>
2018-01-12 22:51:09 +0800

    Edit QueueDataGraphic.cs
    Edit QueueDataGraphic.cs

--- QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
+++ QueueDataGraphic/QueueDataGraphic/CSharpFiles/QueueDataGraphic.cs
@@ -24,7 +24,12 @@ namespace QueueDataGraphic.CSharpFiles
 		/// <param name="ChannelCount">the number of graphic channels, 繪圖通道數量</param>
 		public QueueDataGraphic(int ChannelCount)                               //	QueueDataGraphic constructor, QueueDataGraphic建構子
 		{                                                                       //	QueueDataGraphic constructor start, 進入QueueDataGraphic建構子
-
+			DataQueueList = new List<DataQueue>();                              //	initialize DataQueueList, 初始化DataQueueList物件
+			for (int Loopnum = 0; Loopnum < ChannelCount; Loopnum++)			//	add graphic channels, 加入繪圖通道
+			{																	//	for loop start, 進入for迴圈
+				DataQueueList.Add(new DataQueue());                             //	add graphic channels, 加入繪圖通道
+			}																	//	for loop end, 結束for迴圈
 		}                                                                       //	QueueDataGraphic constructor end, 結束QueueDataGraphic建構子
+
 	}                                                                           //	QueueDataGraphic class end, 結束QueueDataGraphic類別
 }                                                                               //	namespace end, 結束命名空間
