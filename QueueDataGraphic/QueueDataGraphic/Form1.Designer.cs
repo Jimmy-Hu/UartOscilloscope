85c5cf87fc533a7ecc5abce5a8e83a3de4557f29
jimmy hu <60071jimmy@gmail.com>
2018-01-13 12:29:47 +0800

    Add panel1 object
    Add panel1 object

--- QueueDataGraphic/QueueDataGraphic/Form1.Designer.cs
+++ QueueDataGraphic/QueueDataGraphic/Form1.Designer.cs
@@ -28,13 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(808, 386);
+			this.panel1.TabIndex = 0;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(832, 410);
+			this.Controls.Add(this.panel1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -42,6 +51,8 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
