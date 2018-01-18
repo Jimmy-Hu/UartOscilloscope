d69fc4da705464e72969e6723207c189d2e3698a
jimmy hu <60071jimmy@users.noreply.github.com>
2018-01-18 23:42:41 +0800

    Add comment
    Add comment

--- UartOscilloscope/CSharpFiles/DataQueue.cs
+++ UartOscilloscope/CSharpFiles/DataQueue.cs
@@ -6,7 +6,7 @@
  ********************************************************************
  */
 
-using System;
+using System;                                                                   //	Using System library
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
