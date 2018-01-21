c79a910daa62fd5ae0fa09bd7cda1ed525c30b80
jimmy hu <60071jimmy@gmail.com>
2018-01-21 18:52:02 +0800

    Edit GraphicData object
    Edit GraphicData object

--- VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/CSharpFiles/DataArray.cs
+++ VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/CSharpFiles/DataArray.cs
@@ -2,7 +2,7 @@
  * Develop by Jimmy Hu												*
  * This program is licensed under the Apache License 2.0.			*
  * DataArray.cs														*
- * 本檔案建立繪圖資料佇列單元物件									*
+ * 本檔案建立繪圖資料陣列單元物件									*
  ********************************************************************
  */
 
@@ -18,9 +18,9 @@ namespace ArrayDataGraphic.CSharpFiles
 	{                                                                           //	DataArray class start, 開始DataArray類別
 		/// <summary>
 		/// GraphicData is the queue for storing the graphic data.
-		/// GraphicData佇列用於儲存繪圖資料
+		/// GraphicData陣列用於儲存繪圖資料
 		/// </summary>
-		private Queue<object> GraphicData;                                      //	GraphicData object, GraphicData佇列物件
+		private object[] GraphicData;											//	GraphicData object, GraphicData陣列物件
 
 		/// <summary>
 		/// DataArrayName is the name of DataArray.
@@ -40,7 +40,7 @@ namespace ArrayDataGraphic.CSharpFiles
 		/// <param name="NewDataArrayName">為DataArray名稱</param>
 		public DataArray(string NewDataArrayName)                               //	DataArray constructor, DataArray建構子
 		{                                                                       //	DataArray constructor start, 進入DataArray建構子
-			this.GraphicData = new Queue<object>();                             //	initialize GraphicData, 初始化GraphicData物件
+			this.GraphicData = new ArraySegment<object>();                      //	initialize GraphicData, 初始化GraphicData物件
 			this.DataArrayName = NewDataArrayName;                              //	initialize DataArrayName, 初始化DataArrayName字串
 			this.GraphicDataArrayMax = 100;                                     //	initialize GraphicDataArrayMax, 初始化GraphicDataArrayMax變數
 		}                                                                       //	DataArray constructor end, 結束DataArray建構子
@@ -50,9 +50,9 @@ namespace ArrayDataGraphic.CSharpFiles
 		/// </summary>
 		/// <param name="NewDataArrayName">為DataArray名稱</param>
 		/// <param name="NewGraphicDataArrayMax">為GraphicDataArray資料儲存數量上限值</param>
-		public DataArray(string NewDataArrayName, int NewGraphicDataArrayMax)                            //	DataArray constructor, DataArray建構子
+		public DataArray(string NewDataArrayName, int NewGraphicDataArrayMax)   //	DataArray constructor, DataArray建構子
 		{                                                                       //	DataArray constructor start, 進入DataArray建構子
-			this.GraphicData = new Queue<object>();                             //	initialize GraphicData, 初始化GraphicData物件
+			this.GraphicData = new ArraySegment<object>();                      //	initialize GraphicData, 初始化GraphicData物件
 			this.DataArrayName = NewDataArrayName;                              //	initialize DataArrayName, 初始化DataArrayName字串
 			this.GraphicDataArrayMax = NewGraphicDataArrayMax;                  //	initialize GraphicDataArrayMax, 初始化GraphicDataArrayMax變數
 		}                                                                       //	DataArray constructor end, 結束DataArray建構子
@@ -84,27 +84,13 @@ namespace ArrayDataGraphic.CSharpFiles
 		}                                                                       //	GetGraphicDataArrayMax method end, 結束GetGraphicDataArrayMax方法
 
 		/// <summary>
-		/// RemovingOverload method would remove the data when GraphicData.count > GraphicDataArrayMax
-		/// RemovingOverload用於清除多於物件數量上限之資料
-		/// </summary>
-		private void RemovingOverload()                                         //	RemovingOverload method, RemovingOverload方法
-		{                                                                       //	RemovingOverload method start, 進入RemovingOverload方法
-			while(GraphicData.Count > GraphicDataArrayMax)                      //	when GraphicData.Count more than GraphicDataArrayMax, 當有過多資料
-			{                                                                   //	while loop start, 進入while迴圈
-				GraphicData.Dequeue();                                          //	clear GraphicData data, 清除GraphicData資料
-			}                                                                   //	while loop end, 結束while迴圈
-			return;																//	return, 結束方法
-		}                                                                       //	RemovingOverload method end, 結束RemovingOverload方法
-
-		/// <summary>
 		/// AddData method would add new data into queue.
 		/// AddData方法用於存入新資料至Queue
 		/// </summary>
 		/// <param name="InputData">欲紀錄至Queue之資料</param>
 		public void AddData(object InputData)                                   //	AddData method, AddData方法
 		{                                                                       //	AddData method start, 進入AddData方法
-			GraphicData.Enqueue(InputData);                                     //	add data into GraphicData, 新增資料至GraphicData佇列
-			RemovingOverload();                                                 //	call RemovingOverload method, 呼叫RemovingOverload方法
+			GraphicData.Enqueue(InputData);                                     //	add data into GraphicData, 新增資料至GraphicData陣列
 		}                                                                       //	AddData method end, 結束AddData方法
 
 		/// <summary>
@@ -112,7 +98,7 @@ namespace ArrayDataGraphic.CSharpFiles
 		/// GetGraphicData方法用於回傳GraphicData物件
 		/// </summary>
 		/// <returns>GraphicData object, GraphicData物件</returns>
-		public Queue<object> GetGraphicData()                                   //	GetGraphicData method, GetGraphicData方法
+		public ArraySegment<object> GetGraphicData()                            //	GetGraphicData method, GetGraphicData方法
 		{                                                                       //	GetGraphicData method start, 進入GetGraphicData方法
 			return GraphicData;                                                 //	return GraphicData object, 回傳GraphicData物件
 		}                                                                       //	GetGraphicData method end, 結束GetGraphicData方法
