6f42b16095756ba461982ddb693ad98817dad41a
jimmy hu <60071jimmy@gmail.com>
2018-01-21 18:50:46 +0800

    Add comment
    Add comment

--- VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/CSharpFiles/DataArray.cs
+++ VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/CSharpFiles/DataArray.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/********************************************************************
+ * Develop by Jimmy Hu												*
+ * This program is licensed under the Apache License 2.0.			*
+ * DataArray.cs														*
+ * 本檔案建立繪圖資料佇列單元物件									*
+ ********************************************************************
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,8 @@ using System.Threading.Tasks;
 
 namespace ArrayDataGraphic.CSharpFiles
 {                                                                               //	namespace start, 進入命名空間
-	class DataArray
-	{
-	}
+	class DataArray                                                             //	DataArray class, DataArray類別
+	{                                                                           //	DataArray class start, 開始DataArray類別
+
+	}                                                                           //	DataArray class end, 結束DataArray類別
 }                                                                               //	namespace end, 結束命名空間
