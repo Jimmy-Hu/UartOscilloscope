6d8cbf35af5536b185087c340353c46eed02bc7e
jimmy hu <60071jimmy@gmail.com>
2018-01-21 19:16:51 +0800

    Add panel1 object
    Add panel1 object

--- VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/Form1.Designer.cs
+++ VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/Form1.Designer.cs
@@ -28,13 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(13, 13);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(259, 236);
+			this.panel1.TabIndex = 0;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.panel1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +52,8 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
