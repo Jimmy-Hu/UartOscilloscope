ea2b0958b26a42b55dbd78c08b4549f11b5414ac
jimmy hu <60071jimmy@gmail.com>
2018-01-21 19:16:17 +0800

    Edit Form1_Load method
    Edit Form1_Load method

--- VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/Form1.cs
+++ VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/Form1.cs
@@ -19,7 +19,27 @@ namespace ArrayDataGraphic
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			//	Add channels
+			List<string> TestList = new List<string>();
+			TestList.Add("Channel1");
+			TestList.Add("Channel2");
+			TestList.Add("Channel3");
 
+			//	Create ArrayDataGraphic object
+			CSharpFiles.ArrayDataGraphic ArrayDataGraphic1 = new CSharpFiles.ArrayDataGraphic(TestList);
+
+			//	Add datas
+			for (int Loopnum = 0; Loopnum < 500; Loopnum++)
+			{
+				ArrayDataGraphic1.AddData("Channel1", Loopnum * 2);
+			}
+
+			//	Set graph width and height
+			ArrayDataGraphic1.SetWidth(panel1.Size.Width);
+			ArrayDataGraphic1.SetHeight(panel1.Size.Height);
+
+			panel1.Paint += new PaintEventHandler(ArrayDataGraphic1.DrawGraph);
+			panel1.Refresh();
 		}
 	}
 }
