7d11be69e177d1e170c733ede0cef1602729400a
jimmy hu <60071jimmy@gmail.com>
2018-01-21 18:31:03 +0800

    Create project
    Create project

---
+++ VisualStudioProjects/ArrayDataGraphic/ArrayDataGraphic/CSharpFiles/DataArray.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDataGraphic.CSharpFiles
+{                                                                               //	namespace start, 進入命名空間
+	class DataArray
+	{
+	}
+}                                                                               //	namespace end, 結束命名空間
